ee088eb6642c0fee664e438f855e4631d5441f19
root <root@DDNS.lan>
2024-09-08 02:11:02 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const { spawn } = require('child_process');
+const fs = require("fs");
+const fspromise = require('fs').promises;
+
+const { parse } = require("csv-parse");
+const { stringify } = require("csv-stringify");
+const util = require('util');
+
+
+async function resultFile() {
+    const fsOpen = util.promisify(fs.open);
+    const fsTruncate = util.promisify(fs.ftruncate);
+    const fsClose = util.promisify(fs.close);
+
+    const fd = await fsOpen("./iptest.csv", 'r+');
+    const writableStream = fs.createWriteStream("./iptest.csv");
+
+    await fsTruncate(fd, 0);
+    await fsClose(fd);
+    console.log('文件内容已清空。');
+
+
+    fs.createReadStream("./result.csv")
+        .pipe(parse({ delimiter: ",", from_line: 2 }))
+        .on("data", function (row) {
+            console.log(row);
+            const str = row[0] + ":443" + "#本地测速" + "\n";
+            fs.appendFile('./iptest.csv', str, (err) => { })
+        })
+        .on("end", function () {
+            console.log("finished");
+        })
+        .on("error", function (error) {
+            console.log(error.message);
+        });
+}
+
+const cfstCmd = './CloudflareST';
+const cfstCmdParams = '-url https://speed.songxunlei.uk/ -t 8 -dn 10 -sl 8';
+const cfstSpawn = spawn(cfstCmd, cfstCmdParams.split(' '));
+
+
+cfstSpawn.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+cfstSpawn.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+cfstSpawn.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+
+    fs.readFile('./result.csv', async (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        await resultFile()
+    })
+});
